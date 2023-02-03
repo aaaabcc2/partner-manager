a054cfe2b512b841cfa8cb8a4db5f0efdd9949a4
fish <3303077472@qq.com>
2023-02-03 14:33:06 +0800

    后台管理系统搭建

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn,
+  })
+app.mount('#app')
